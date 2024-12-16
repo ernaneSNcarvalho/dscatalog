f2bc5c0726562468ad5dc7c4cb5fc077f0d20e53
ernane <ernanecarvalho89@gmail.com>
2024-12-16 12:36:35 -0300

    h2 database

---
+++ backend/src/main/java/com/dscatalog/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.dscatalog.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dscatalog.dscatalog.entities.Category;
+import com.dscatalog.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
