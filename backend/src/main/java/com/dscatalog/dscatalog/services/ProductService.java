7d599b9505fad8d260e9bc3a6a1b7630addb6631
Ernane <ernanecarvalho89@gmail.com>
2025-02-25 12:56:06 -0300

    erro no teste

--- backend/src/main/java/com/dscatalog/dscatalog/services/ProductService.java
+++ backend/src/main/java/com/dscatalog/dscatalog/services/ProductService.java
@@ -4,8 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,8 +31,8 @@ public class ProductService {
 	private CategoryRepository categoryRepository;
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-		Page<Product> list = repository.findAll(pageRequest);
+	public Page<ProductDTO> findAllPaged(Pageable pageable){
+		Page<Product> list = repository.findAll(pageable);
 		return list.map(x -> new ProductDTO(x));
 	}
 
@@ -66,12 +67,13 @@ public class ProductService {
 	public void delete(Long id) {
 		try {
 			repository.deleteById(id);
-		}catch(EntityNotFoundException e) {
-			throw new DataBaseException("Data base!");
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
 		}catch(DataIntegrityViolationException e) {
-			throw new DataBaseException("Integrity violation");
+			throw new DataBaseException("Integrity violation! ");
 		}
 	}
+
 	
 	private void copyDtoToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
