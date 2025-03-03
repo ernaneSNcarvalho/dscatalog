08573a3afa56afe405cb699889fd5737ec9c1d7a
ernane <ernanecarvalho89@gmail.com>
2025-03-03 13:54:59 -0300

    Testes

---
+++ backend/src/test/java/com/dscatalog/dscatalog/repositories/Factory.java
@@ -0,0 +1,20 @@
+package com.dscatalog.dscatalog.repositories;
+
+import java.time.Instant;
+
+import com.dscatalog.dscatalog.dto.ProductDTO;
+import com.dscatalog.dscatalog.entities.Category;
+import com.dscatalog.dscatalog.entities.Product;
+
+public class Factory {
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.00, "https://img.com/img.png", Instant.parse("2020-10-20T03:00:00Z"));
+		product.getCategories().add(new Category(2L, "Electronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+}
--- backend/src/test/java/com/dscatalog/dscatalog/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/dscatalog/dscatalog/repositories/ProductRepositoryTests.java
@@ -3,10 +3,10 @@ package com.dscatalog.dscatalog.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.EmptyResultDataAccessException;
 
 import com.dscatalog.dscatalog.entities.Product;
 
@@ -16,22 +16,48 @@ public class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository repository;
 	
+	private long existingId;
+	private long nonExistingId;
+	private long countTotalProducts;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	
 	@Test
-	public void deleteShouldDeleteObjectWhenIdExists() {
-		long existingId = 1L;
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		Product product = Factory.createProduct();
+		product.setId(null);
+		product = repository.save(product);
 		
-		repository.deleteById(existingId);
-		
+		Assertions.assertNotNull(product.getId());
+		Assertions.assertEquals(countTotalProducts + 1, product.getId());
+	}
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {		
+		repository.deleteById(existingId);		
 		Optional<Product> result = repository.findById(existingId);
 		Assertions.assertFalse(result.isPresent());
 	}
 	
 	@Test
-	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {
-		long nonExistingId = 1000L;
-		
-		Assertions.assertThrows(EmptyResultDataAccessException.class, () -> {
-			repository.deleteById(nonExistingId);
-		});
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenIdDoesNotExist() {		
+		Assertions.assertFalse(repository.existsById(nonExistingId));
+	}
+	
+	@Test
+	public void searchforproductidshouldreturnanoptionalproductwhentheidexists() {
+		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertTrue(result.isPresent());
+	}
+	
+	@Test
+	public void searchforproductidshouldreturnanoptionalproductwhentheidDoNotexists() {
+		Optional<Product> result = repository.findById(nonExistingId);
+		Assertions.assertFalse(result.isPresent());
 	}
 }
